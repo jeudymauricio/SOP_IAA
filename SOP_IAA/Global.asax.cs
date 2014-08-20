affb3682ea4724f8c9568e257c03d01406afde77
jeudymauricio <jeudymauricio@gmail.com>
2014-08-20 12:43:54 -0600

    asdasd

--- SOP_IAA/Global.asax.cs
+++ SOP_IAA/Global.asax.cs
@@ -16,6 +16,7 @@ namespace SOP_IAA
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
