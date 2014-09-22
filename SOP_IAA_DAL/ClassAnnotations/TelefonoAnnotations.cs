18d2af32d39f4d252036c5c47bd4aeaba1a2cb03
jeudymauricio <jeudymauricio@gmail.com>
2014-09-22 15:36:03 -0600

    Footer 2

---
+++ SOP_IAA_DAL/ClassAnnotations/TelefonoAnnotations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOP_IAA_DAL
+{
+    [MetadataType(typeof(telefonoMetaData))]
+    public partial class telefono
+    {
+    }
+
+    public class telefonoMetaData
+    {
+
+        [DisplayName("Teléfono")]
+        [StringLength(9)]
+        [RegularExpression("[1-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]")]
+        public string telefono1 { get; set; }
+    }
+}
