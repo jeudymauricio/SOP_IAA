93dd1af175dbba90b881cec38361de56213ec2c5
nspinozam <nspinozam@gmail.com>
2014-10-03 16:18:25 -0600

    ItemRejuste Index

---
+++ SOP_IAA/Controllers/Mis_Controladores/ItemReajusteController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SOP_IAA_DAL;
+
+namespace SOP_IAA.Controllers
+{
+    public partial class ItemReajusteController : Controller
+    {
+        //GET: itemReajuste/Periodo
+        public ActionResult Periodo(int? periodo)
+        {
+            if (periodo == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return View();
+        }
+
+        // POST: itemReajustes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,idContratoItem,fecha,mes,ano,reajuste,precioReajustado")] itemReajuste itemReajuste)
+        {
+            if (ModelState.IsValid)
+            {
+                db.itemReajuste.Add(itemReajuste);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idContratoItem = new SelectList(db.contratoItem, "id", "id", itemReajuste.idContratoItem);
+            return View(itemReajuste);
+        }
+
+        // POST: itemReajustes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,idContratoItem,fecha,mes,ano,reajuste,precioReajustado")] itemReajuste itemReajuste)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(itemReajuste).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idContratoItem = new SelectList(db.contratoItem, "id", "id", itemReajuste.idContratoItem);
+            return View(itemReajuste);
+        }
+
+        // POST: itemReajustes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            itemReajuste itemReajuste = db.itemReajuste.Find(id);
+            db.itemReajuste.Remove(itemReajuste);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ SOP_IAA_DAL/ClassAnnotations/ItemReajusteAnnotations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOP_IAA_DAL
+{
+    [MetadataType(typeof(itemReajusteMetaData))]
+    public partial class itemReajuste
+    {
+    }
+
+    public class itemReajusteMetaData
+    {
+        [Required]
+        [DisplayName("Fecha")]
+        public System.DateTime fecha { get; set; }
+
+        [DisplayName("Mes")]
+        public Nullable<int> mes { get; set; }
+
+        [DisplayName("Año")]
+        public Nullable<int> ano { get; set; }
+
+        [Required]
+        [DisplayName("Reajuste")]
+        public decimal reajuste { get; set; }
+
+        [Required]
+        [DisplayName("Precio Reajustado")]
+        public decimal precioReajustado { get; set; }
+
+        public virtual contratoItem contratoItem { get; set; }
+
+    }
+}
